a5e0c7aa08461c67cc1bf02aeba93d6fa0bd3060
ferros <michael@ferrossystems.com>
2018-06-16 12:19:20 -0700

    completed anagrams

--- anagram_checker.py
+++ anagram_checker.py
@@ -14,11 +14,13 @@ def num_del(first_string, second_string):
     for character, occur_count in first_string.items():
         try:
             diff_between_chars = occur_count - second_string[character]
+            second_string[character] = occur_count
         except KeyError:
             diff_between_chars = occur_count
+
         num_of_deletions_needed += abs(diff_between_chars)
 
-    return num_of_deletions_needed
+    return num_of_deletions_needed, second_string
 
 
 if __name__ == '__main__':
@@ -26,9 +28,10 @@ if __name__ == '__main__':
 
     b = 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'
 
-    string_one = OrderedDict(sorted(Counter(a).items()))
-    string_two = OrderedDict(sorted(Counter(b).items()))
+    string_one = Counter(a)
+    string_two = Counter(b)
 
-    delete_these = num_del(string_one, string_two) + num_del(string_two, string_one)
+    delete_these, string_two = num_del(string_one, string_two)
+    delete_that, _ = num_del(string_two, string_one)
 
-    print(delete_these)
+    print(delete_these + delete_that)
