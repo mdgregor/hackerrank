7575feb6ef927bcd8de1c54bb42b977a73bfa750
ferros <michael@ferrossystems.com>
2018-07-04 08:46:38 -0700

    final solution for balanced brackets

--- stacks.py
+++ stacks.py
@@ -8,32 +8,29 @@ import sys
 from collections import Counter
 
 
+def is_balanced(expression):
+    balance = []
+    curly, brace, paren = ('{', '}'), ('[', ']'), ('(', ')')
+
+    balanced_brackets = True
+
+    for index, char in enumerate(expression):
+        try:
+            if char in (curly[0], brace[0], paren[0]) and expression[index + 1]:
+                balance.append(char)
+            elif char in (curly[1], brace[1], paren[1]):
+                if (balance.pop(), char) not in [curly, brace, paren]:
+                    balanced_brackets = False
+
+        except IndexError:
+            balanced_brackets = False
+
+    return balanced_brackets
+
+
 if __name__ == '__main__':
     t = int(1)
     for t_itr in range(t):
         expression = list('[]([][])(){}{{}{[](){}}}()[]({})[{}')
 
-        balance = []
-        curly = ['{', '}']
-        brace = ['[', ']']
-        paren = ['(', ')']
-        left = ['{', '[', '(']
-        right = ['}', ']', ')']
-
-        balanced_brackets = True
-
-        for char in expression:
-            if char in left:
-                balance.append(char)
-            elif char in right:
-                if [balance.pop(), char] not in [curly, brace, paren]:
-                    balanced_brackets = False
-
-            if not balanced_brackets:
-                break
-
-        print('YES') if balanced_brackets else print('NO')
-
-
-
-
+        print('YES') if is_balanced(expression) else print('NO')
